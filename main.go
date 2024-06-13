a65af69195ec45fca443c93332a7fc5281b8cbaa
Christopher Lim <toffer.lim87@gmail.com>
2024-06-12 23:49:06 -0700

    - add cors

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
     "github.com/clim-bot/todo-list-api/config"
     "github.com/clim-bot/todo-list-api/models"
     "github.com/clim-bot/todo-list-api/routes"
+    "github.com/gin-contrib/cors"
 )
 
 
@@ -15,6 +16,16 @@ func main() {
     db.AutoMigrate(&models.User{}, &models.Task{})
 
     router := gin.Default()
+
+    // CORS middleware
+    router.Use(cors.New(cors.Config{
+        AllowOrigins:     []string{"http://localhost:3000"},
+        AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+        AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+        ExposeHeaders:    []string{"Content-Length"},
+        AllowCredentials: true,
+    }))
+
     routes.SetupRoutes(router, db)
     router.Run(":8080")
 }
